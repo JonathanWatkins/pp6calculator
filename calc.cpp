6488d882526b9937a0a9ea3c813631147ea7bb25
jon <jon@jon-mint-laptop.(none)>
2013-02-18 16:45:32 +0000

    Stable version with modular functions

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <sstream>
+#include <string.h>
+#include <climits>
+
+
 
 using namespace std;
 
-// Just a quick bit on passing using pointers
+// Just a quick bit on passing using pointerst
+
+double get_double (string question) {
+	if (question.empty()) {
+	 question="Enter number: ";
+	}
+	
+	// Add error checking
+	double a;
+	cout << question;
+	
+	while (!(cin >> a)) {
+		cin.clear();
+		cin.ignore(INT_MAX,'\n');
+		cout << question;
+		
+	};
+
+}
+
+
 void f(int *a)
 {
 	int answer= *a *2;
@@ -10,6 +35,71 @@ void f(int *a)
 	
 	
 }
+
+double multiply(){
+	
+	stringstream oss;
+	
+	double a= get_double(NULL);
+	double b=get_double(NULL);
+	oss << a << " * " << b << " = " << a*b;
+	cout << oss.str();
+	
+	
+}
+
+double divide(){
+
+	stringstream oss;
+	
+	double a= get_double("");
+	double b=get_double("");
+	oss << a << " / " << b << " = " << a/b;
+	cout << oss.str();
+	
+	
+}
+
+double add(){
+
+	stringstream oss;
+	
+	double a= get_double("");
+	double b=get_double("");
+	oss << a << " + " << b << " = " << a+b;
+	
+	cout << oss.str();
+}
+
+double subtract(){
+
+	stringstream oss;
+	
+	double a= get_double("");
+	double b=get_double("");
+	oss << a << " - " << b << " = " << a-b;
+	
+	cout << oss.str();
+	
+}
+
+double intercept(){
+
+	stringstream oss;
+	
+	
+	double m= get_double("m = ");
+	
+	double c=get_double("c = ");
+	
+	oss << "The intercept of the line with the x axis is " << (0-c)/m;
+	
+	cout << oss.str();
+	
+}
+
+
+
 
 int main() {
 	
@@ -24,34 +114,61 @@ int main() {
 	double a;
 	double b;
 	
-	cout << "What operation do you want to perform ?" << endl;
-	cout << "1)   +" << endl;
-	cout << "2)   -" << endl;
-	cout << "3)   *" << endl;
-	cout << "4)   /" << endl;
+	char operation[1];
+	char ReturnStr[50];
 	
-	int operation;
+	stringstream optionsOss;
+	
+	optionsOss << "What operation do you want to perform ?" << endl;
+	optionsOss << "'+' '-' '*' '/' 'i' or 'q' to quit" << endl;
+		
 	
 	do
 	{
-		if (!(cin >> operation)){
-			cout << "This is not a valid choice!  Please choose again." << endl; 
-			cout << "What operation do you want to perform ?" << endl;
-			cout << "1)   +" << endl;
-			cout << "2)   -" << endl;
-			cout << "3)   *" << endl;
-			cout << "4)   /" << endl;
+	
+		cout << optionsOss.str();
+		
+		
+		do
+		{
+			
+			cin >> operation;
+			
+			if (( *operation!='+' && *operation!='-' && *operation!='*' && *operation!='/'  && *operation!='q' && *operation!='i')) {
+				cin.clear();
+				cin.ignore();
+				cout << "\nThis is not a valid choice!  Please choose again." << endl; 
+				cout << optionsOss.str();
+			} else {
+			
+				cout << "You have chosen " << operation << endl;
+			}
+	
 		}
-	
+		while ( *operation!='+' && *operation!='-' && *operation!='*' && *operation!='/' && *operation!='q' && *operation!='i');
+		
+		if (*operation!='q') {
+			
+			double answer;
+			if (*operation=='+') {
+				add();
+			} else if (*operation=='-') {
+				subtract();	
+			} else if (*operation=='*') {
+				multiply();
+			} else if (*operation=='/') {
+				divide();
+			} else if (*operation=='i') {
+				intercept();
+			}
+			
+			
+			cout << "\n\n";
+			
+		}
+		
 	}
-	while ( operation>=5 || operation <=0 );
-	cout << "Enter a number: ";
-	cin >> a;
-	cout << endl;
-	
-	cout << "Enter a number: ";
-	cin >> b;
-	cout << endl << a << " + " << b << " = "  << a+b << endl;
+	while (*operation!='q');
 	
 	
 	return 0;
