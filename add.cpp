c624d2ebb384e7502b29270a8bb43e230688952c
jon <jon@jon-mint-laptop.(none)>
2013-02-18 14:04:43 +0000

    Small program to add two numbers

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	double a;
+	double b;
+	
+	cout << "Enter a number: ";
+	cin >> a;
+	cout << endl;
+	
+	cout << "Enter a number: ";
+	cin >> b;
+	cout << endl << a << " + " << b << " = "  << a+b << endl;
+	
+	
+	return 0;
+}
