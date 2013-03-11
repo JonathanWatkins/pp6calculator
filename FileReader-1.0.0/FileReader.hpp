3cc88e7a8d7936bb7b083c98bd3acc75adb30758
Jonathan Watkins <jmtrik@gmail.com>
2013-03-11 12:12:19 +0000

    Added templates and pdg.dbt - extra bits left out in first commit

--- FileReader-1.0.0/FileReader.hpp
+++ FileReader-1.0.0/FileReader.hpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <string>
+#include <iostream>
 
 class FileReader {
  public:
@@ -15,6 +16,40 @@ class FileReader {
   float getFieldAsFloat(const int n);
   double getFieldAsDouble(const int n);
   std::string getFieldAsString(const int n);
+
+	template <typename T> 
+	T getField(const int n, bool &failed) {
+		
+		failed = false;
+		std::istringstream ist(line);
+		this->skip_fields(ist, n-1);
+		T rval;
+		ist >> rval;
+		if (ist.fail()) {
+			failed = true;
+			std::cout << "Problem reading file" << std::endl;
+			return rval;
+		}
+		else
+			return rval;
+	};
+	
+	/*
+	float FileReader::getFieldAsFloat(const int n) {
+	 failed = false;
+	  std::istringstream ist(line);
+	  this->skip_fields(ist, n-1);
+	  float rval;
+	  ist >> rval;
+	  if (ist.fail()) {
+	    failed = true;
+	    return 0.0;
+	  }
+	  else
+	    return rval;
+	}
+	*/
+	
 
   bool inputFailed() const;
   bool isValid() const;
